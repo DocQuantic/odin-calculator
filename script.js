c78a7805d6310af1fa264f43a3425a436ca54d0e
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-20 16:31:00 +0100

    Add basic operations functions and variables
    Operand variable is an array of objects containing operation name,
    symbol and function.
    This setup will allow to access operations by simply using find() method
    on the array.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Hello, World!")
+/** Variables **/
+let operatorA = 0;
+let operatorB = 0;
+const operands = [
+    {
+        name: "addition",
+        symbol: "+",
+        function: add,
+    },
+    {
+        name: "subtraction",
+        symbol: "-",
+        function: subtract,
+    },
+    {
+        name: "multiplication",
+        symbol: "*",
+        function: multiply,
+    },
+    {
+        name: "division",
+        symbol: "/",
+        function: divide,
+    },
+]
+
+/* operands.find(op => op.symbol==="+").function(1, 2) */
+
+/** Basic operations functions **/
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
