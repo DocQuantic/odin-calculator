bd8d67705422c27f1cf7e85e71786ede3fac99d5
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-24 09:50:42 +0100

    Add comments for chain calculation logic

--- script.js
+++ script.js
@@ -82,6 +82,10 @@ const operandBtns = document.querySelectorAll(".operand");
 for(button of operandBtns){
     button.addEventListener("click", (e) => {
         if(operator === null){
+            /* 
+            *  After pressing equal, if a number is pressed, operand1 is set to null
+            *  in order start the calculation from the beginning
+            */
             operand1 = null;
         }
         if(textToDisplay === null){
@@ -98,16 +102,28 @@ for(button of operatorBtns){
     button.addEventListener("click", (e) => {
         if(getDisplay() !== ""){
             if(operand1 === null && operator === null){
+                /*
+                *  If operand1 and operator are null, the calculation starts from the beginning
+                *  and pressing operator sets operand1 and operator
+                */
                 operand1 = parseFloat(textToDisplay);
                 operator = e.target.textContent;
                 textToDisplay = null;
             } else if(operand1 !== null && operator !== null){
+                /*
+                *  If operand1 and operator are not null, pressing operator sets operand 2 and performs 
+                *  calculation. It also sets the operator for the next calculation.
+                */
                 operand2 = parseFloat(getDisplay());
                 operand1 = operate(operator, operand1, operand2);
                 setDisplay(operand1.toString());
                 textToDisplay = null;
                 operator = e.target.textContent;
             } else if(operand1 !== null && operator === null){
+                /* 
+                *  If operand1 is not null and operator is null, it means that last calculation was performed 
+                *  by pressing equal button. In this case, operator and operand2 are set. But the calculation is not performed.
+                */
                 operator = e.target.textContent;
                 operand2 = parseFloat(getDisplay());
                 textToDisplay = null;
